bb4c756418d8e9c11a554a42391d6b299f403dae
kouha.shu <k-syuu@msn.coj>
2018-09-28 18:37:10 +0900

    add static files

--- src/gae/main.go
+++ src/gae/main.go
@@ -4,18 +4,26 @@ import (
 	"fmt"
 	"google.golang.org/appengine"
 	"net/http"
+	"os"
 )
 
-func handle(w http.ResponseWriter,r *http.Request) {
-	if r.URL.Path != "/" {
-		http.Redirect(w,r,"/",http.StatusFound)
-		return
-	}
+func hello(w http.ResponseWriter, r *http.Request) {
+	////redirect
+	//if r.URL.Path != "/" {
+	//	http.Redirect(w,r,"/",http.StatusFound)
+	//	return
+	//}
 
-	fmt.Fprintln(w,"Hello Wolrd!")
+	fmt.Fprintln(w, "Hello Wolrd!")
+
+	if envVar := os.Getenv("MY_VAR"); envVar != "" {
+		fmt.Fprint(w, "I get an envioremnt var :"+envVar)
+	} else {
+		fmt.Fprint(w, "I get none envioremnt var")
+	}
 }
 
 func main() {
-	http.HandleFunc("/",handle)
+	http.HandleFunc("/hello", hello)
 	appengine.Main()
 }
